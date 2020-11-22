fec8f1d85deaa4fdb988174d0e8f0d096d0d6a61
perceive123 <626510958@qq.com>
2020-11-22 22:33:27 +0800

    补交

--- Week05/homework1/Douban/Douban/settings.py
+++ Week05/homework1/Douban/Douban/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'jx7qof$)-+x_tlv1#mf95!8=g_x=9y&(^ae7)##$7$-vb%t##t'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Douban.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -84,7 +81,6 @@ DATABASES = {
         'PORT': '3306',
     },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -104,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -117,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
--- Week05/homework1/Douban/shorts/views.py
+++ Week05/homework1/Douban/shorts/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from .models import Shorts
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 
 def shorts(request):
-    infos = Shorts.objects.filter(stars__gt=3).order_by('-stars')#倒序
+    infos = Shorts.objects.filter(stars__gt=3).order_by('-stars')  # 倒序
     if 'q' in request.GET:
         infos = Shorts.objects.filter(shorts__contains=request.GET.get(
             'q')).order_by('-stars')  # 写法2  infos = Shorts.objects.filter(shorts__contains=request.GET['q'])
     return render(request, 'shorts.html', locals())
+
 
 def index(request):
     infos = Shorts.objects.all().order_by('-stars')
